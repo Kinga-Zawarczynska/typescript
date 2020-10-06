1920204e21ded9baefc414549ad0c677e9fc6bba
Kinga <zawarczynska.ko@gmail.com>
2020-10-06 20:26:51 +0200

    feat(ts): objects and arrays in ts

--- sandbox.ts
+++ sandbox.ts
@@ -1,12 +1,33 @@
-let character = 'mario';
-let age = 30;
-let isBlackBelt = false;
+let names = ['luigi', 'mario', 'yoshi'];
 
-// character = 50;
-character = 'luigi';
+names.push('tobe');
+// names.push(3);
+// names[0] = true;
+let numbers = [10, 20, 30, 40];
+numbers.push[2];
+// numbers.push('=string');
 
-const circ = (diameter: number) => {
-  return diameter * Math.PI;
+let mixed = ['ken', 4, 'chun-li', 8, 9, true];
+mixed.push('hello');
+mixed.push(false);
+mixed[0] = 3;
+
+
+
+//objects
+let ninja = {
+  name: 'mario',
+  belt: 'black',
+  age: 30
 }
 
-console.log(circ(3))
+ninja.age = 40;
+// ninja.name = 3;
+ninja.name = 'ryan';
+// ninja.skills = ['fighting', 'sneaking']
+// ninja = 'hello';
+ninja = {
+  name: 'yoshi',
+  belt: 'orange',
+  age: 40
+}
