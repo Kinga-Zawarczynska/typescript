3612898b3c63ecbdd34a3ff2166dce771d702513
Kinga <zawarczynska.ko@gmail.com>
2020-10-22 13:40:58 +0200

    feat(ts): lesson 4

--- sandbox.js
+++ sandbox.js
@@ -1,9 +1,26 @@
-var character = 'mario';
-var age = 30;
-var isBlackBelt = false;
-// character = 50;
-character = 'luigi';
-var circ = function (diameter) {
-    return diameter * Math.PI;
-};
-console.log(circ(3));
+var character;
+var age;
+// age = 'luigi'
+age = 13;
+character = 'han solo';
+// character = false;
+//arrays
+var ninjas;
+// or we can declare empty array right away, because above there is no array, it will be created in future
+var superNinjas = [];
+// ninjas = [10, 23]
+ninjas = ['han', 'solo'];
+//union types
+var mixed = [];
+mixed.push('hello');
+mixed.push(4);
+// mixed.push(false);
+var uid;
+uid = '123';
+uid = 1223;
+// uid = false;
+// objects
+var ninjaOne;
+ninjaOne = { name: 'yoshi', age: 30 };
+var ninjatwo;
+ninjatwo = { name: 'mario', age: 20, belt: 'black' }; //but we can't leave it empty object
